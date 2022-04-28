a9ca1233f158f5d8c861b383c751476491e60d8d
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-04-29 01:45:59 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,12 @@
+/* 
+
+// -------------- One Time For Your Computer -------
+// --> 01. Create Heroku account (One Time)
+// --> 02. Verify Email (One Time)
+// --> 03. install heroku cli (One Time)
+// --> 04. heroku login (One Time)
+
+----------------- Each Time for your project -----------
+// --> 01. heroku create
+
+*/
